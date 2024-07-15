94d89075c09640d39d9673ce9ecb314b56bbf965
Gui <168383419+Guilherme220501@users.noreply.github.com>
2024-07-15 04:07:59 -0300

    upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() { var pageTitle = document.title;
+if (pageTitle === "Sobre MIM !!"){
+    alert("Seja Bem-vindo a minha Página Oficial!!");
+} else if (pageTitle === "Minha 2ªPágina!!!") {
+    alert("Seja Bem-vindo a minha 2ªPágina!");
+} 
+};
+
+
